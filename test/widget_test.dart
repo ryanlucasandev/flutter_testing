e71d5bd594f699f4d2e311989d6c3c81d6d8be29
ryanlucasandev <ryan.lucasan.dev@gmail.com>
2026-05-07 14:13:03 +0800

    Find widgets in widget testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_testing/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('MyWidget has a title and message', (tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(const MyWidget(title: 'T', message: 'M'));
+    final titleFinder = find.text('T');
+    final messageFinder = find.text('M');
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Use the 'findsOneWidget' matcher provided by flutter_test to verify
+    // that the Text widgets appear exactly once in the widget tree.
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
   });
+
+  testWidgets('finds a Text widget', (tester) async {
+    // Build an App with a Text widget that displays the letter 'H'.
+    await tester.pumpWidget(const MaterialApp(home: Scaffold(body: Text('H'))));
+    expect(find.text('H'), findsOneWidget);
+  });
+
+  testWidgets('finds a widget using a Key', (tester) async {
+    // Defind the test key.
+    const testKey = Key('K');
+
+    // Build a MaterialApp with the testKey.
+    await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
+
+    // Find the MaterialAP widget using the testKey.
+    expect(find.byKey(testKey), findsOneWidget);
+  });
+
+  testWidgets('finds a specific instance', (tester) async {
+    const childWidget = Padding(padding: EdgeInsets.zero);
+
+    /// Provide the childWidget to the Container.
+    await tester.pumpWidget(Container(child: childWidget));
+
+    // Search for the childWidget in the tree and verify it exists.
+    expect(find.byWidget(childWidget), findsOneWidget);
+  });
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key, required this.title, required this.message});
+
+  final String title;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(title: Text(title)),
+        body: Center(child: Text(message)),
+      ),
+    );
+  }
 }
