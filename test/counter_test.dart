73e8de0bc25f9a019648be38d3b95676584484d9
ryanlucasandev <ryan.lucasan.dev@gmail.com>
2026-05-07 14:02:10 +0800

    flutter testing with test and group

---
+++ test/counter_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing/counter.dart';
+
+void main() {
+  group('Test start, increment, decrement', () {
+    test('Counter should start at 0', () {
+      final counter = Counter();
+      expect(counter.value, 0);
+    });
+
+    test('Counter value should be incremented', () {
+      final counter = Counter();
+      counter.increment();
+      expect(counter.value, 1);
+    });
+
+    test('Counter value should be decremented', () {
+      final counter = Counter();
+      counter.decrement();
+      expect(counter.value, -1);
+    });
+  });
+}
