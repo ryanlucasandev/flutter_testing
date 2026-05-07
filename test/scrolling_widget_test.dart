db8b11e1f0dcc34a1ad4ac088c0687b353084d27
ryanlucasandev <ryan.lucasan.dev@gmail.com>
2026-05-07 14:09:02 +0800

    handling scrolling in widget tests

---
+++ test/scrolling_widget_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing/scrolling_widget.dart';
+
+void main() {
+  testWidgets('finds a deep item in a long list', (tester) async {
+    // Build our app and trigger a frame
+    await tester.pumpWidget(
+      MyApp(items: List<String>.generate(10000, (i) => 'Item $i')),
+    );
+
+    final listFinder = find.byType(Scrollable);
+    final itemFinder = find.byKey(const ValueKey('item_50_text'));
+
+    // Scroll until the itme to be found appears
+    await tester.scrollUntilVisible(itemFinder, 500.0, scrollable: listFinder);
+
+    // Verify that the item contains the correct text
+    expect(itemFinder, findsOneWidget);
+  });
+}
