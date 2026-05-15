f345d640887fc52f4088f778452e20ecb9be1efb
ryanlucasandev <ryan.lucasan.dev@gmail.com>
2026-05-15 13:52:49 +0800

    add linter bloc_lint

--- lib/mock_test/fetch_data.dart
+++ lib/mock_test/fetch_data.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 Future<Album> fetchAlbum() async {
--- lib/widget_test/orientation_widget.dart
+++ lib/widget_test/orientation_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_testing/main.dart';
 
 void main() {
   runApp(MyApp());
